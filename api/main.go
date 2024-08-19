7fb81a6fb41b8e24f97e5be967ce0ac07ee7e4df
h-masano <hiroki.masanon@gmail.com>
2024-08-19 22:44:18 +0900

    add sample GET and PUT method

--- api/main.go
+++ api/main.go
@@ -1,13 +1,50 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/h-masano/ent-api-sample/database/ent"
+	_ "github.com/lib/pq"
+)
 
 func main() {
+
+	// create db client with ent
+	client, err := ent.Open("postgres", "host=ent-api-sample-db-1 port=5432 user=root dbname=sampledb password=password sslmode=disable")
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+	// get all users
+	r.GET("/users", func(c *gin.Context) {
+		users, err := client.Users.Query().All(c)
+		if err != nil {
+			c.JSON(500, gin.H{"error": err})
+			return
+		}
+		c.JSON(200, users)
 	})
-	r.Run()
+
+	// put user
+	r.PUT("/user", func(c *gin.Context) {
+		age := c.Query("age")
+		name := c.Query("name")
+
+		ageInt, err := strconv.Atoi(age)
+		if err != nil {
+			c.JSON(400, gin.H{"error": "age must be integer"})
+			return
+		}
+
+		user, err := client.Users.Create().SetAge(ageInt).SetName(name).Save(c)
+		if err != nil {
+			c.JSON(500, gin.H{"error": err})
+			return
+		}
+		c.JSON(200, user)
+	})
 }
